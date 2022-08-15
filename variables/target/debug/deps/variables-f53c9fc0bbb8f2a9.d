fb1b296593f1ad45e584be4e327bab9f46fb84f0
thiago <thiagosteps@gmail.com>
2022-08-15 18:25:14 +0100

    variables not working

---
+++ variables/target/debug/deps/variables-f53c9fc0bbb8f2a9.d
@@ -0,0 +1,5 @@
+/home/thiagosteps227/rust-learnings/variables/target/debug/deps/variables-f53c9fc0bbb8f2a9: src/main.rs
+
+/home/thiagosteps227/rust-learnings/variables/target/debug/deps/variables-f53c9fc0bbb8f2a9.d: src/main.rs
+
+src/main.rs:
